c49326dc6e3cf04bb94aa0d9e3e610831d50c95d
HrishitaPanda <hrishitapanda1152@gmail.com>
2026-07-13 10:47:00 +0530

    Jump_2

---
+++ hello_world/Jump_2.java
@@ -0,0 +1,27 @@
+public class Jump_2 {
+
+    public static int jump(int[] nums) {
+        int jumps = 0;
+        int currentEnd = 0;
+        int farthest = 0;
+
+        // No need to jump from the last index
+        for (int i = 0; i < nums.length - 1; i++) {
+            farthest = Math.max(farthest, i + nums[i]);
+
+            // End of the current jump's range
+            if (i == currentEnd) {
+                jumps++;
+                currentEnd = farthest;
+            }
+        }
+
+        return jumps;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 3, 1, 1, 4};
+
+        System.out.println("Minimum jumps: " + jump(nums));
+    }
+}
